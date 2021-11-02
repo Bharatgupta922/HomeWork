f7f85158649a879aec870845fa96e190948b1f07
Bharatgupta922 <gbharat922@gmail.com>
2021-11-02 17:26:46 +0530

    new project added for recursion and backtracking

---
+++ Recursions/ConvertForms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursions
+{
+    public static class ConvertForms
+    {
+        public static string DecimalToBin(int n)
+        {
+            if (n > 0)
+            {
+                return n%2 +  DecimalToBin(n / 2) ;
+            }
+            return "";
+        }
+    }
+}
---
+++ Recursions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recursions // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            //int input = GetInput();
+            Console.WriteLine($"Decimal {9} to binary {ConvertForms.DecimalToBin(11)}");
+            
+        }
+
+        private static int GetInput()
+        {
+            Console.WriteLine("Input the string ");
+            return Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
