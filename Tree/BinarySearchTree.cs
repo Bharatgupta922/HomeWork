d2828027a17a5c70621a3097e5eac51e863501bf
Bharatgupta922 <gbharat922@gmail.com>
2021-11-13 23:51:50 +0530

    tree data structure insert and traversal of BST

---
+++ Tree/BinarySearchTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public  class BinarySearchTree
+    {
+        public TreeNode InsertNode(TreeNode root, TreeNode node)
+        {
+
+            if(root == null)
+            {
+                root = node;
+            }
+            else if (root.data > node.data)
+            {
+                root.left = InsertNode(root.left , node);
+            }
+            else
+            {
+                root.right = InsertNode(root.right,node);
+            }
+
+            return root;
+        }
+        
+        public void preOrder(TreeNode root)
+        {
+            if (root == null) return;
+            Console.Write($"{root.data} ,");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+        public void inOrder(TreeNode root)
+        {
+            if (root == null) return;
+            inOrder(root.left);
+            Console.Write($"{root.data} ,");
+            inOrder(root.right);
+        }
+        public void postOrder(TreeNode root)
+        {
+            if (root == null) return;
+            postOrder(root.left);
+            postOrder(root.right);
+            Console.Write($"{root.data} ,");
+        }
+
+
+
+    }
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tree // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(10);
+           BinarySearchTree bst = new BinarySearchTree();
+            bst.InsertNode(root , new TreeNode(4));
+            bst.InsertNode(root, new TreeNode(5));
+            bst.InsertNode(root, new TreeNode(19));
+            bst.InsertNode(root, new TreeNode(14));
+            bst.InsertNode(root, new TreeNode(13));
+            bst.inOrder(root);
+            Console.WriteLine();
+            bst.preOrder(root);
+            Console.WriteLine();
+            bst.postOrder(root);
+            Console.WriteLine();
+
+        }
+
+        private static int GetInput()
+        {
+            Console.WriteLine("Input the string ");
+            return Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
